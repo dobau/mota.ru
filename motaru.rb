0df4e7f0291990fe85e9c4e29e729787029b1617
Rafael Alves <do_bau@hotmail.com>
2010-10-19 01:04:25 -0200

    Criado projeto mota.ru

---
+++ motaru.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+require 'net/http'
+
+#####################################
+# Configuração
+#####################################
+# Link do mota.ru que possui o thumb das imagens
+link = 'http://www.mota.ru/en/categories/view/name/nature'
+# Pasta onde as imagens serão salvas
+folder = 'C:\Users\dobau\Documents\My Dropbox\Photos\wallpaper'
+# Resolução da imagem ()
+resolution = '1280x1024'
+
+
+
+# Método para criar a pasta onde as imagens devem ser salvas se não existe
+def create_folder(directory_name)
+	Dir::mkdir(directory_name) if not FileTest::directory?(directory_name)
+end
+
+# Cria um arquivo se o mesmo ainda não existir
+def create_file(resp, file_name)
+	if !FileTest::file?(file_name)
+		open(file_name, 'wb' ) { |file|
+			file.write(resp.body)
+		}
+	end
+end
+
+# Recupera as imagens de Thumb
+pageThumb = Hpricot(open( URI::escape("http://www.mota.ru/en/categories/view/name/nature") ))
+imageElements = pageThumb.search('img[@class=wallpaperThumb]')
+
+if (!imageElements.empty?)
+	create_folder(folder);
+
+	puts "Salvando:"
+	# Percorre todas os 'thumbs' encontrados, baixa a foto adicionando /resolution/1280x1024 a imagem
+	imageElements.each {|imageElement|
+		id = imageElement.parent.get_attribute('href').split('/').last
+		puts id
+		
+		pageDownload = Hpricot(open( URI::escape("http://www.mota.ru/en/wallpapers/get/id/#{id}/resolution/#{resolution}") ))
+		src = pageDownload.search('img[@class=wallpaper]').first.get_attribute('src')
+		ext = src.split('.').last
+		#puts src		
+
+		url = URI::parse(src)
+		req = Net::HTTP::Get.new(url.path)
+		Net::HTTP.start(url.host, url.port) {|http|
+			resp = http.request( req )
+			create_file(resp, "#{folder}/#{id}.#{ext}")			
+		}
+		#puts 'Sucesso.'
+	}
+end
