8715a23bc5009a210dc4cce9d7c3df70647404e0
Rafael Alves <do_bau@hotmail.com>
2011-09-28 16:24:42 -0300

    I fixed xpath to retrieve images after updates of the mota.ru

--- motaru.rb
+++ motaru.rb
@@ -4,23 +4,23 @@ require 'open-uri'
 require 'net/http'
 
 #####################################
-# Configuração
+# Configuraï¿½ï¿½o
 #####################################
 # Link do mota.ru que possui o thumb das imagens
 tags = ['nature']
-# Pasta onde as imagens serão salvas
+# Pasta onde as imagens serï¿½o salvas
 folder = 'C:\Users\dobau\Pictures\wallpaper'
-# Resolução da imagem ()
+# Resoluï¿½ï¿½o da imagem ()
 resolution = '1280x1024'
 
 
 
-# Método para criar a pasta onde as imagens devem ser salvas se não existe
+# Mï¿½todo para criar a pasta onde as imagens devem ser salvas se nï¿½o existe
 def create_folder(directory_name)
 	Dir::mkdir(directory_name) if not FileTest::directory?(directory_name)
 end
 
-# Cria um arquivo se o mesmo ainda não existir
+# Cria um arquivo se o mesmo ainda nï¿½o existir
 def create_file(resp, file_name)
 	if !FileTest::file?(file_name)
 		open(file_name, 'wb' ) { |file|
@@ -34,7 +34,7 @@ tags.each { |tag|
 	
 	# Recupera as imagens de Thumb
 	pageThumb = Hpricot(open( URI::escape("http://www.mota.ru/en/categories/view/name/#{tag}") ))
-	imageElements = pageThumb.search('img[@class=wallpaperThumb]')
+	imageElements = pageThumb.search("//div[@id='categoryWallpapersList']//img[@class='wallpaperThumb']")
 
 	if (!imageElements.empty?)
 		create_folder(folder);
